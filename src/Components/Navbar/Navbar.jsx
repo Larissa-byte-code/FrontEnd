965f696c90bab674836a92bf1f8d5caf9cf4598d
Larissa <larissa@jm-contacts.net>
2025-07-17 10:46:07 +0300

    Ajout link dans navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
+import { Link } from "react-router-dom";
 const Navbar=() => {
  const [menu,SetMenu]=useState("shop");
 return(
@@ -11,16 +12,16 @@ return(
             <p>SHOPPER</p>
         </div>
         <ul className="nav-menu">
-            <li onClick={()=>{SetMenu("shop")}}>Shop {menu==="shop"?<hr/>:<></>}</li>{/*if menu=shop alors /hr sinon rien*/} 
-            <li onClick={()=>{SetMenu("mens")}}>Men{menu==="mens"?<hr/>:<></>}</li>
-            <li onClick={()=>{SetMenu("womens")}}>Women{menu==="womens"?<hr/>:<></>}</li>
-            <li onClick={()=>{SetMenu("kids")}}>Kids{menu==="kids"?<hr/>:<></>}</li>
+            <li onClick={()=>{SetMenu("shop")}}><Link to='/'>Shop</Link> {menu==="shop"?<hr/>:<></>}</li>{/*if menu=shop alors /hr sinon rien*/} 
+            <li onClick={()=>{SetMenu("mens")}}><Link to='/mens'>Men</Link> {menu==="mens"?<hr/>:<></>}</li>
+            <li onClick={()=>{SetMenu("womens")}}><Link to='/womens'>Women</Link>{menu==="womens"?<hr/>:<></>}</li>
+            <li onClick={()=>{SetMenu("kids")}}><Link to='/kids'>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
         </ul>
        
         <div className="nav-login-cart">
-            <button>Login</button>
+            <Link to='/login'><button>Login</button></Link>
             <div className="cart-wrapper">
-            <img src={cart_icon} alt="" />
+            <Link to='/cart'><img src={cart_icon} alt="" /></Link>
             <div className="nav-cart-count">0</div>
         </div>
             
