e71573503e057eeb62f35464b3fa5fce653068ff
Larissa <larissa@jm-contacts.net>
2025-07-21 14:52:07 +0300

    modif collection

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -3,6 +3,7 @@ import Hero from '../Components/Hero/Hero'
 import Popular from "../Components/Popular/Popular";
 import Offers from "../Components/Offers/Offers";
 import NewCollections from "../Components/NewCollections/NewCollections";
+import NewLetter from "../Components/NewLetter/NewLetter";
 const Shop=() =>{
     return(
         <div>
