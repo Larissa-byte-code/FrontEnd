fc5d6f8f37a50868208d6ea717742763cdab47ba
Larissa <larissa@jm-contacts.net>
2025-07-24 11:03:57 +0300

    Ajout productdisplay dans product

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -19,7 +19,7 @@ export const ProductDisplay = (props) => {
         </div>
         <div className='productdisplay-right'>
             <h1>{product.name}</h1>
-            <div className='productdisplay-right-star'>
+            <div className='productdisplay-right-stars'>
                 <img src={star_icon} alt="" />
                 <img src={star_icon} alt="" />
                 <img src={star_icon} alt="" />
@@ -27,6 +27,31 @@ export const ProductDisplay = (props) => {
                 <img src={star_dull_icon} alt="" />
                 <p>(122)</p>
             </div>
+            <div className='productdisplay-right-prices'>
+                <div className='productdisplay-right-price-old'>
+                    ${product.old_price}
+                </div>
+                <div className='productdisplay-right-price-new'>
+                    $${product.new_price}
+                </div>
+            </div>
+            <div className='productdisplay-right-description'>
+                A lightweight usually knitted, pullover shirt,close-fitting and with a round neckline and
+                short sleeves,worn as an undershirt or outer garment
+            </div>
+            <div className='productdisplay-right-size'>
+                <h1>select size</h1>
+                <div className='productdisplay-right-sizes'>
+                    <div>S</div>
+                    <div>M</div>
+                    <div>L</div>
+                    <div>XL</div>
+                    <div>XXL</div>
+                </div>
+            </div>
+            <button>ADD TO CART</button>
+            <p className='productdisplay-right-category'><span>Category :</span>Women,T-shirt,Crop-top</p>
+            <p className='productdisplay-right-category'><span>Tags :</span>Modern, Latest</p>
         </div>
     </div>
   )
